7d50e3d537e46fd913a1c91d709bfb439e642c94
Max Kos <maxie7@protonmail.com>
2016-04-25 12:14:13 +0300

    Implement methods initialize [] and []=

--- factory.rb
+++ factory.rb
@@ -1,3 +1,4 @@
+# return an anonymous class
 class Factory
   
   def self.new(*method_names, &blok)
@@ -8,6 +9,28 @@ class Factory
     # raise a TypeError for arguments which are not symbols
     method_names.each do |x|
       raise TypeError unless x.kind_of? Symbol
+    end
+    
+    Class.new do
+      # methods defined by the factory
+      include Enumerable
+
+      define_method :initialize do |*args|
+      	@args = Hash[method_names.zip args]
+      end
+
+      define_method "[]".to_sym do |x|
+      	raise NameError, "no member '#{x}' in struct" unless @args.has_key? x.to_sym
+      	@args[x.to_sym]
+      end
+
+      define_method "[]=".to_sym do |x, val|
+      	raise NameError, "no member '#{x}' in struct" unless @args.has_key? x.to_sym
+      	@args[x.to_sym] = val
+      end
+
+      
+
     end
 
   end
