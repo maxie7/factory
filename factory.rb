b5f575a2432b55018c6b43376280180506a61375
Max Kos <maxie7@protonmail.com>
2016-04-25 11:57:37 +0300

    Add raising some errors

--- factory.rb
+++ factory.rb
@@ -1,4 +1,14 @@
 class Factory
   
+  def self.new(*method_names, &blok)
+  	
+  	# raise an ArgumentError if not given at least one argument
+    raise ArgumentError, "wrong number of arguments (0 for 1+)" if method_names.empty?
+    
+    # raise a TypeError for arguments which are not symbols
+    method_names.each do |x|
+      raise TypeError unless x.kind_of? Symbol
+    end
 
+  end
 end
